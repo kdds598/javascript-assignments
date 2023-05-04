0e14c3d8cf483e42efa389b7657528cbb80d6efd
kdds598 <132571304+kdds598@users.noreply.github.com>
2023-05-04 17:37:33 +0530

    Javascript Day 2 Assignment

---
+++ Day_2.js
@@ -0,0 +1,13 @@
+//Write a function that takes in two parameters and returns their difference.
+function difference(a,b)
+{
+
+var diff;  
+
+diff=a-b;
+
+return diff;
+}
+console.log(difference(999,444));
+
+
